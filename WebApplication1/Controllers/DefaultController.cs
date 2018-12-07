0b428c15e8baddf08a4954992cd49539267d02f8
xiaolong2000 <1341034035@qq.com>
2018-12-07 10:17:25 +0800

    88
    99

---
+++ WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string ada()
+        {
+            return "顶顶顶顶";
+        }
+        public Customer getCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerNmae = "asd";
+            ct.Address = "adaa";
+            return ct;
+
+        }
+    }
+}
